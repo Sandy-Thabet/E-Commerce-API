ef3f2ba4fd944f543aaf985ccda374d9e5592018
Sandy-Thabet <sandythabit588@gmail.com>
2024-04-13 20:31:19 +0200

    add logs

--- controllers/Payment/general-payment-controller.js
+++ controllers/Payment/general-payment-controller.js
@@ -2,6 +2,9 @@ const { catchAsync } = require('../../utils/catchAsync');
 const generalPaymentService = require('../../services/Payment/general-payment-service');
 
 exports.handlePaymobCallback = catchAsync(async (req, res, next) => {
+  console.log('BODY = ', req.body);
+  console.log('QUERY = ', req.query);
+
   await generalPaymentService.handlePaymobCallback(
     req.body.obj,
     req.query.hmac
